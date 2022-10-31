b83bb99c66c127951a0b15ea5af2125591bbdfe4
Fusion <fusion@Fusions-Mac-mini.local>
2022-10-31 15:28:56 +0300

    started mytabBar

---
+++ src/navigation/MyTabBar/MyTabBar.tsx
@@ -0,0 +1,63 @@
+import {
+  BottomTabDescriptorMap,
+  BottomTabNavigationEventMap,
+} from '@react-navigation/bottom-tabs/lib/typescript/src/types';
+import {
+  NavigationHelpers,
+  ParamListBase,
+  TabNavigationState,
+} from '@react-navigation/native';
+import {FC} from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+
+type PropsType = {
+  state: TabNavigationState<ParamListBase>;
+  descriptors: BottomTabDescriptorMap;
+  navigation: NavigationHelpers<ParamListBase, BottomTabNavigationEventMap>;
+};
+
+const MyTabBar: FC<PropsType> = ({state, descriptors, navigation}) => {
+  return (
+    <View style={{flexDirection: 'row'}}>
+      {state.routes.map((route, index) => {
+        const {options} = descriptors[route.key];
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            // The `merge: true` option makes sure that the params inside the tab screen are preserved
+            //navigation.navigate({name: route.name, merge: true});
+          }
+        };
+
+        return (
+          <TouchableOpacity
+            accessibilityRole="button"
+            accessibilityState={isFocused ? {selected: true} : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            style={{flex: 1}}>
+            <Text style={{color: isFocused ? '#673ab7' : '#222'}}>{}</Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+};
+
+export default MyTabBar;
